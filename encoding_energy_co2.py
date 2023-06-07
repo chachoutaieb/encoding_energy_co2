ed3fd846531b0225bc81f1bb9755f453b751b8ff
chachoutaieb <107298199+chachoutaieb@users.noreply.github.com>
2023-06-07 23:10:07 +0100

    Update encoding_energy_co2.py

--- encoding_energy_co2.py
+++ encoding_energy_co2.py
@@ -172,9 +172,6 @@ class video_encoding:
         if platform != 'GPU':
             VVenC_path = None
             VVdeC_path = None
-            
-            VVenC_path = '/media/ridha/D81821F01821CE76/Doctorat/Projet4_Energy/CTC/codeP4/vvenc-master/bin/release-static/vvencapp'
-            VVdeC_path = '/media/ridha/D81821F01821CE76/Doctorat/Projet4_Energy/CTC/codeP4/vvdec-master/bin/release-static/vvdecapp'
             """
             Example :
                 VVenC_path = 'path to folder/vvenc-master/bin/release-static/vvencapp'
