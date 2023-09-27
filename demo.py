d663fc864fe191ca6dddf9752de3c7bde286f38a
chachoutaieb <107298199+chachoutaieb@users.noreply.github.com>
2023-09-27 01:11:48 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,19 +11,3 @@ python3 '/path to the project folder/encoding_energy_co2.py' \
       -it  0.04  0.1 0.2 1 0.2 \
       -m psnr ssim vmaf 
 
-
-python3 '/path to the project folder/encoding_energy_co2.py'  \
-      -i '/path to the project folder/original_dataset/BasketballPass_416x240_50.yuv' \
-      -o '/path to the project folder/original_dataset/outputVideo.mp4' \
-      -r '/path to the project folder/encoding_resultsCPU.xlsx' \
-      -s 416x240 \
-      -f 50 \
-      -x 8 \
-      -b 500 \
-      -c x264 \
-      -p CPU \
-      -sp slower \
-      -it 0.3 \
-      -m psnr ssim vmaf
-
-
